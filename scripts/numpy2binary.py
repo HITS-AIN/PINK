12d8c55db22b254dd4198bb39b46d448776efadc
Bernd Doser <bernd.doser@h-its.org>
2014-10-30 09:55:41 +0000

    getopt for numpy2binary.py

--- scripts/numpy2binary.py
+++ scripts/numpy2binary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import getopt
 import Image
 import matplotlib
 import numpy
@@ -7,46 +8,84 @@ import struct
 import sys
 from matplotlib import pyplot
 
-files = sys.argv[1:]
-of = open('result.bin', 'wb')
-
-data = numpy.load(files[0])
-total = data.shape[0]
-height = data.shape[1]
-width = data.shape[2]
-
-for file in files[1:]:
-    data = numpy.load(file)
-    total += data.shape[0]
-    if (height != data.shape[1]):
-        print 'Shape error.'
-        sys.exit(1)
-    if (width != data.shape[2]):
-        print 'Shape error.'
-        sys.exit(1)
+def print_usage():
+    print sys.argv[0], ' -o <outputfile> [inputfiles]'
+    
+if __name__ == "__main__":
+    
+    shuffle = False
+    outputfile = 'result.bin'
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"ho:s",["ofile="])
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(2)
+    
+    for opt, arg in opts:
+        if opt == '-h':
+            print_usage()
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    
+    files = args
+    of = open(outputfile, 'wb')
+    
+    data = numpy.load(files[0])
+    total = data.shape[0]
+    height = data.shape[1]
+    width = data.shape[2]
+    
+    for file in files[1:]:
+        data = numpy.load(file)
+        total += data.shape[0]
+        if (height != data.shape[1]):
+            print 'Shape error.'
+            sys.exit(1)
+        if (width != data.shape[2]):
+            print 'Shape error.'
+            sys.exit(1)
+            
+    of.write(struct.pack('i', total))
+    of.write(struct.pack('i', height))
+    of.write(struct.pack('i', width))
+    
+    data = numpy.load(files[0])
+    
+    if shuffle:
+        print 'Shuffle'
+        for file in files[1:]:
         
-of.write(struct.pack('i', total))
-of.write(struct.pack('i', height))
-of.write(struct.pack('i', width))
+            print 'Input file is ', file
+        
+            data = numpy.concatenate((data,numpy.load(file)))
+        
+            print 'data.ndim = ', data.ndim
+            print 'data.shape = ', data.shape
+            print 'data.size = ', data.size
+        
+        numpy.random.shuffle(data)
+        
+        data.astype('f').tofile(of)
+    else:
+        print 'No shuffle'
+        for file in files[1:]:
+        
+            print 'Input file is ', file
+        
+            data = numpy.load(file)
+        
+            print 'data.ndim = ', data.ndim
+            print 'data.shape = ', data.shape
+            print 'data.size = ', data.size
 
-data = numpy.load(files[0])
+            data.astype('f').tofile(of)
 
-for file in files[1:]:
-
-    print 'Input file is ', file
-
-    data = numpy.concatenate((data,numpy.load(file)))
-
-    print 'data.ndim = ', data.ndim
-    print 'data.shape = ', data.shape
-    print 'data.size = ', data.size
-
-numpy.random.shuffle(data)
-
-data.astype('f').tofile(of)
-
-of.close()
-
-print 'All done.'
-sys.exit(0)
-
+    of.close()
+    
+    print 'All done.'
+    sys.exit(0)
+    
