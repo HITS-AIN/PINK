5177a4e1d655e7754a381a443f2d0469a7975d73
Bernd Doser <bernd.doser@h-its.org>
2014-11-10 09:17:33 +0000

    numpy2binary: handle NaN's in input data

--- scripts/numpy2binary.py
+++ scripts/numpy2binary.py
@@ -10,30 +10,36 @@ def print_usage():
     print '<command> -o <outputfile> [inputfiles]'
     
 if __name__ == "__main__":
-    
-    shuffle = False
+
     norm = False
     outputfile = 'result.bin'
-    
+    BrokenImageBehavior = 'Skip'
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"ho:s",["ofile="])
+        opts, args = getopt.getopt(sys.argv[1:],"ho:nb:",["ofile=","norm","broken="])
     except getopt.GetoptError:
         print_usage()
-        sys.exit(2)
-    
+        sys.exit(1)
+
     for opt, arg in opts:
         if opt == '-h':
             print_usage()
             sys.exit()
-        elif opt == "-s":
-            shuffle = True
         elif opt in ("-o", "--ofile"):
             outputfile = arg
-    
+        elif opt in ("-n", "--norm"):
+            norm = True
+        elif opt in ("-b", "--broken"):
+            if arg not in ['Skip', 'SetToZero']:
+                print 'Unkown option for broken ', arg
+                sys.exit(1)
+            BrokenImageBehavior = arg
+
     files = args
     of = open(outputfile, 'wb')
 
     total = 0
+    nbBrokenImages = 0
     max_height = 0
     min_height = sys.maxint
     max_width = 0
@@ -41,8 +47,15 @@ if __name__ == "__main__":
 
     for file in files:
         data = numpy.load(file)
-        total += data.shape[0]
         for i in xrange(len(data)):
+
+            image = data[i]
+
+            if numpy.isnan(image).any():
+                nbBrokenImages += 1
+                if BrokenImageBehavior == 'Skip':
+                    continue
+
             if (data[i].shape[0] > max_height):
                 max_height = data[i].shape[0]
             if (data[i].shape[0] < min_height):
@@ -52,7 +65,10 @@ if __name__ == "__main__":
             if (data[i].shape[1] < min_width):
                 min_width = data[i].shape[1]
 
-    print 'number = ', total
+        total += data.shape[0]
+
+    print 'Number of images = ', total
+    print 'Number of images containing NaN elements = ', nbBrokenImages
     print 'max_height = ', max_height
     print 'min_height = ', min_height
     print 'max_width  = ', max_width
@@ -75,14 +91,16 @@ if __name__ == "__main__":
 
             image = data[i]
 
+            if numpy.isnan(image).any():
+                if BrokenImageBehavior == 'Skip':
+                    continue
+                elif BrokenImageBehavior == 'SetToZero':
+                    image = numpy.nan_to_num(image)
+
             if norm:
                 image = 1.0 * image / numpy.max(image)
-                indices = numpy.where(image<numpy.std(image)*3.0)
+                indices = numpy.where(image < numpy.std(image)*3.0)
                 image[indices] = 0
-
-            if numpy.isnan(image.any()):
-                print 'image entry is nan.'
-                sys.exit(1)
 
             if (image.shape[0] != dim or image.shape[1] != dim):
                 image[(image.shape[0]-dim)/2:(image.shape[0]+dim)/2, (image.shape[1]-dim)/2:(image.shape[1]+dim)/2].astype('f').tofile(of)
@@ -90,7 +108,7 @@ if __name__ == "__main__":
                 image.astype('f').tofile(of)
 
     of.close()
-    
+
     print 'All done.'
     sys.exit(0)
 
