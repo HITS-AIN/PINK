bda314e3402432f69a76e28ce6f190e7265fb066
Erica Hopkins <erica.hopkins@h-its.org>
2017-09-11 14:04:38 +0000

    Ref: #1103 Implementing new visualization capabilities for hex shapes.

--- scripts/showHeatmap.py
+++ scripts/showHeatmap.py
@@ -1,78 +1,168 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
 import getopt
 import numpy
-import matplotlib.pylab as plt
+import matplotlib
+#imports pyplot in code for control over backend
 import struct
 import sys
+from math import ceil, floor
 
 def print_usage():
-    print ''
-    print 'Usage:'
-    print ''
-    print '  showHeatmap.py [Options] <inputfile>'
-    print ''
-    print 'Options:'
-    print ''
-    print '  --help, -h              Print this lines.'
-    print '  --image, -i <int>       Number of image to visualize (default = 0).'
-    print ''
+    print ('')
+    print ('Usage:')
+    print ('')
+    print ('  showHeatmap.py [Options] <inputfile>')
+    print ('')
+    print ('Options:')
+    print ('')
+    print ('  --help, -h              Print this lines.')
+    print ('  --image, -i <int>       Number of image to visualize (default = 0).')
+    print ('  --border, -b <int>      Border size.')
+    print ('  --neuron, -n <int>      Neuron size.')
+    print ('  --save, -s <String>     Location to save JPGs to.')
+    print ('  --name, -n <String>     Name of saved file. Channel number will automatically be added to the end of the name.')
+    print ('  --display, -d <int>     1 to display SOM as well as save it. Default 0.')
+    print ('')
 
 if __name__ == "__main__":
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hi:",["help", "image="])
+        opts, args = getopt.getopt(sys.argv[1:],"h:i:b:n:s:n:d:",["help", "image=", "border=", "neuron=", "save=", "name=", "display="])
     except getopt.GetoptError:
         print_usage()
         sys.exit(1)
 
+    #Default parameters
     imageNumber = 0
     channelNumber = 0
+    border = 1
+    neuronSize = 5
+    save = ""
+    name = "heatmap"
+    display = 0
 
+    #Set parameters
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print_usage()
             sys.exit()
         elif opt in ("-i", "--image"):
             imageNumber = int(arg)
+        elif opt in ("-b", "--border"):
+            border = int(arg)
+        elif opt in ("-n", "--neuron"):
+            neuronSize = int(arg)
+        elif opt in ("-s", "--save"):
+            save = arg
+        elif opt in ("-n", "--name"):
+            name = arg
+        elif opt in ("-d", "--display"):
+            display = int(arg)
 
     if len(args) != 1:
         print_usage()
-        print 'ERROR: Input file is missing.'
+        print ('ERROR: Input file is missing.')
         sys.exit(1)
 
     inputfile = args[0]
 
-    print 'Input file is ', inputfile
-    print 'Image number is ', imageNumber
+    print ('Input file is ', inputfile)
+    print ('Image number is ', imageNumber)
+    print ('Border size is ', border)
+    print ('Neuron size is ', neuronSize)
+    if(display==1):
+        print ('Display is on')
+    else:
+        print ('Display is off')
+
+    #Importing here allows control of the backend depending on if display is on or off.
+    if(display==0):
+        matplotlib.use('Agg')
+    import matplotlib.pylab as plt
 
     inFile = open(inputfile, 'rb')
     numberOfImages, SOM_width, SOM_height, SOM_depth = struct.unpack('i' * 4, inFile.read(4*4))
 
-    print 'Number of images = ', numberOfImages 
-    print 'SOM_width = ', SOM_width
-    print 'SOM_height = ', SOM_height
-    print 'SOM_depth = ', SOM_depth
+    print ('Number of images = ', numberOfImages)
+    print ('SOM_width = ', SOM_width)
+    print ('SOM_height = ', SOM_height)
+    print ('SOM_depth = ', SOM_depth)
 
     if imageNumber >= numberOfImages:
-        print 'Image number too large.'
+        print ('Image number too large.')
         sys.exit(1)
 
+    #Size if the map is hexagonal
+    hexSize=int(1.0 + 6.0 * (( (SOM_width-1)/2.0 + 1.0) * (SOM_width-1)/ 4.0))
+    #Size if the map is quadratic
     size = SOM_width * SOM_height * SOM_depth
     image_width = SOM_width
     image_height = SOM_depth * SOM_height
     inFile.seek(imageNumber * size * 4, 1)
     array = numpy.array(struct.unpack('f' * size, inFile.read(size * 4)))
     data = numpy.ndarray([SOM_width, SOM_height, SOM_depth], 'float', array)
-    data = numpy.swapaxes(data, 0, 2)
-    data = numpy.reshape(data, (image_height, image_width))
 
-    fig = plt.figure()
+    #Checks if the map is hexagonal. If hexagonal, all neurons between hexSize and size will be identical
+    if numpy.min(data.flatten()[hexSize:]) - numpy.max(data.flatten()[hexSize:]) == 0:
+        print ("hex heatmap")
+        #Sets size to hexSize, including borders
+        size = [int(ceil(SOM_width * ((neuronSize*2+1) + border) + neuronSize/2.0 + 1)),
+                int(SOM_height * (neuronSize + neuronSize/2.0 + 1 + border) )]
+        image = numpy.empty(size)
+        image[:] = numpy.NAN
+        data = data.reshape(SOM_width*SOM_height, -1)
+
+        #Creates each neuron and fills it with the same color
+        def fillHexagon(x, y, value):
+            for yPos in range(-neuronSize, neuronSize+1):
+                if yPos <= -neuronSize / 2.0:
+                    xs = range(-(neuronSize+yPos)*2, (neuronSize+yPos)*2+1)
+                elif yPos > neuronSize / 2.0:
+                    xs = range(-(neuronSize-yPos)*2, (neuronSize-yPos)*2+1)
+                else:
+                    xs = range(-neuronSize, neuronSize+1)
+                for xPos in xs:
+                    image[int((x + y%2/2.0) * ((neuronSize*2+1) + border) + xPos + neuronSize/2.0),
+                          int(y * ((neuronSize+floor(neuronSize/2.0)+1) + border) + yPos + neuronSize + 1)] = value
+
+        y = 0
+        x = int(SOM_width / 4.0 + 1)
+        for part in data[:hexSize]:
+            #Fills in the "center box" of each mini hexagon
+            fillHexagon(x, y, part)
+            x = x + 1
+            #Fills in the "top triangle"
+            if y < floor(SOM_height / 2.0) and (x>SOM_width - (floor(SOM_width / 4.0) + 1 - floor(y/2.0))):
+                y = y + 1
+                x = int(floor(SOM_width / 4.0) + 1 - ceil(y/2.0))
+            #Fills in the "bottom triangle"
+            elif (x>SOM_width - floor( (y-floor(SOM_height/2.0)) /2.0) - 1 ):
+                y = y + 1
+                x = int(ceil( (y-floor(SOM_height/2.0)) /2.0))
+        #Transposes the data
+        data = image.T
+    else:
+        #If the map is quadratic, swapping the axes and then transposing the data creates the heat map
+        print ("box heatmap")
+        data = numpy.swapaxes(data, 0, 2)
+        data = numpy.reshape(data, (image_height, image_width)).T
+
+    #Saves the heat map
+    fig = plt.figure(figsize=(8,5))
     ax = fig.add_subplot(1,1,1)
     ax.set_aspect('equal')
-    plt.imshow(data, interpolation='nearest', cmap=plt.cm.jet)
+    plt.imshow(data, interpolation='nearest', cmap=plt.cm.get_cmap("jet"))
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.axis("off")
     plt.colorbar()
-    plt.show()
+    fileName = save+'/'+name+'.pdf'
+    plt.savefig(fileName)
+    if(display==1):
+        plt.show()
 
-    print 'All done.'
+    print ('All done.')
     sys.exit()
--- scripts/showSOM.py
+++ scripts/showSOM.py
@@ -1,94 +1,237 @@
 #!/usr/bin/python
 
-import getopt
-import numpy
-import matplotlib.pylab as plt
+from __future__ import print_function
+
 import struct
+import numpy
+import matplotlib
+#imports pyplot in code for control over backend
+import math
+import getopt
 import sys
 
 def print_usage():
-    print ''
-    print 'Usage:'
-    print ''
-    print '  showSOM.py [Options] <inputfile>'
-    print ''
-    print 'Options:'
-    print ''
-    print '  --channel, -c <int>      Number of channel to visualize (default = 0).'
-    print '  --help, -h               Print this lines.'
-    print '  --hex                    Hexagonal layout.'
-    print ''
+    print ('')
+    print ('Usage:')
+    print ('')
+    print ('  showSOM.py [Options] <inputfile>')
+    print ('')
+    print ('Options:')
+    print ('')
+    print ('  --help, -h                   Print this lines.')
+    print ('  --save, -s <String>          Location to save JPGs to.')
+    print ('  --intensity, -i <boolean>    Turn shareIntensity off.')
+    print ('  --border, -b <int>           Border size.')
+    print ('  --color, -c <String>         Background color. [Defaults to \\#ffaadd - HIGHLY RECOMMENDED!!!]')
+    print ('  --name, -n <String>          Name of saved file. Channel number will automatically be added to the end of the name.')
+    print ('  --display, -d <int>          1 to display SOM as well as save it. Default 0.')
+    print ('')
+
+class MAPVisualizer():
+    def __init__(self, fileName):
+        self.__fileName = fileName
+        self.__numberOfChannels = 0
+        self.__somWidth = 0
+        self.__somHeight = 0
+        self.__somDepth = 0
+        self.__neuronWidth = 0
+        self.__neuronHeight = 0
+        self.__neurons = []
+
+    #Returns number of channels
+    def checkChannels(self):
+        inputStream = open(self.__fileName, 'rb')
+        self.__numberOfChannels = struct.unpack("i", inputStream.read(4))[0]
+        return self.__numberOfChannels
+
+    #Reads in map data
+    def readMap(self):
+        #Unpacks the map parameters
+        inputStream = open(self.__fileName, 'rb')
+        self.__numberOfChannels = struct.unpack("i", inputStream.read(4))[0]
+        self.__somWidth = struct.unpack("i", inputStream.read(4))[0]
+        self.__somHeight = struct.unpack("i", inputStream.read(4))[0]
+        self.__somDepth = struct.unpack("i", inputStream.read(4))[0]
+        self.__neuronWidth = struct.unpack("i", inputStream.read(4))[0]
+        self.__neuronHeight = struct.unpack("i", inputStream.read(4))[0]
+
+        print ("channels: " + str(self.__numberOfChannels))
+        print ("width: " + str(self.__somWidth))
+        print ("height: " + str(self.__somHeight))
+        print ("depth: " + str(self.__somDepth))
+        print ("neurons: " + str(self.__neuronWidth) +"x" + str(self.__neuronHeight))
+        #Unpacks data
+        try:
+            while True:
+                data = numpy.ones(self.__neuronWidth * self.__neuronHeight * self.__numberOfChannels)
+                for i in range(self.__neuronWidth * self.__neuronHeight * self.__numberOfChannels):
+                    data[i] = struct.unpack_from("f", inputStream.read(4))[0]
+                self.__neurons.append(data)
+
+        except:
+            inputStream.close()
+        self.__neurons = numpy.array(self.__neurons)
+
+        print (str(len(self.__neurons)) + " neurons loaded")
+
+    #Checks if hexagonal or quadratic map is used
+    def isHexMap(self):
+        return len(self.__neurons) < self.__somHeight * self.__somWidth
+
+    def calculateMap(self, neurons, shareIntensity = False, border = 0, shape="box"):
+        #For quadratic map, it reads through the data and creates each neuron as a 1D array and then resizes it to the neuronSize
+        if shape == "box":
+            neuronSize = numpy.array([self.__neuronWidth, self.__neuronHeight])
+            mapSize = numpy.array([self.__somWidth, self.__somHeight])
+            size = numpy.multiply(mapSize,numpy.array(neuronSize) + border) + border
+            image = numpy.empty(size)
+            image[:] = numpy.NAN
+            for x in range(self.__somWidth):
+                for y in range(self.__somHeight):
+                    data = neurons[x + y*self.__somWidth].reshape(self.__neuronWidth, self.__neuronHeight)
+                    if not shareIntensity:
+                        if numpy.max(data)-numpy.min(data) != 0:
+                            data = 1.0 * (data - numpy.min(data)) / (numpy.max(data) - numpy.min(data))
+                    image[x*(self.__neuronWidth + border): (x+1) * (self.__neuronWidth + border) -border, y * (self.__neuronHeight + border): (y+1) * (self.__neuronHeight + border) - border] = numpy.fliplr(data)
+        #For hexagonal map, it goes through each neuron while accounting for the hexagonal shape, starting at the bottom left corner
+        if shape == "hex":
+            size = numpy.multiply((self.__somWidth+0.5,self.__somHeight),numpy.array((self.__neuronWidth, self.__neuronHeight)) + border) + border
+            size[1] = math.ceil(size[1] - (self.__somHeight-1.0) * self.__neuronHeight / 4.0)
+            size[0] = math.ceil(size[0])
+            size = numpy.asarray(size, int)
+            image = numpy.empty(size)
+            image[:] = numpy.NAN
+            mapY = 0
+            mapX = abs((self.__somHeight-1)/2 - mapY)
+            mapX = mapX / 2 + mapX % 2 - 1
+            for neuron in neurons:
+                mapX = mapX + 1
+                off = abs((self.__somHeight-1)/2 - mapY)
+                if mapX >= self.__somWidth - math.floor(off / 2) - off % 2 * (mapY) % 2:
+                    mapY = mapY + 1
+                    mapX = abs((self.__somHeight-1)/2 - mapY)
+                    mapX = math.floor(mapX / 2) + mapX % 2 * (1-mapY) % 2
+                if mapY >= self.__somHeight:
+                    print("abort")
+                    return image
+                if not shareIntensity:
+                    if numpy.max(neuron)-numpy.min(neuron) != 0:
+                        neuron = 1.0 * (neuron - numpy.min(neuron)) / (numpy.max(neuron) - numpy.min(neuron))
+                for xPos in range(self.__neuronWidth):
+                    for yPos in range(self.__neuronHeight):
+                        if math.floor(xPos/2.0 + yPos) < self.__neuronHeight / 4.0 or \
+                           math.floor(xPos/2.0 + yPos + 1) > self.__neuronWidth + self.__neuronHeight / 4.0 or \
+                           math.floor(xPos/2.0 - yPos + 1) > self.__neuronHeight / 4.0 or \
+                           math.floor(xPos/2.0 - yPos) < -self.__neuronWidth + self.__neuronHeight / 4.0:
+                            continue
+                        else:
+                            x = int(math.floor((1.0 * numpy.floor(mapX) + (mapY%2) / 2.0) * (self.__neuronWidth + border) + xPos + border))
+                            y = int(math.floor(1.0 * mapY * (self.__neuronHeight * 3.0 / 4.0 + border) + yPos + border))
+                            image[x][y] = neuron[xPos + yPos * self.__neuronWidth]
+        return image
+
+    #Creates the SOM and saves it to the specified location. Displays map if --display, -d is set to 1.
+    def showMap(self, channel, shareIntensity = True, borderWidth = 2, facecolor = '#ffaadd'):
+        if facecolor != "#ffaadd":
+            print ("WARNING! using non recommended background color! The results will look ugly.")
+        figure = pyplot.figure( figsize=(16, 16))
+        figure.patch.set_alpha(0.0)
+        figure.patch.set_facecolor(facecolor)
+        pyplot.subplots_adjust(left = 0.01, right = 0.99, bottom = 0.01, top = 0.99, wspace = 0.1, hspace = 0.1)
+        start=int(len(self.__neurons[0]) / self.__numberOfChannels * channel)
+        end=int(len(self.__neurons[0]) / self.__numberOfChannels * (channel+1))
+        if self.isHexMap():
+            print ("hexagonal map")
+            image = self.calculateMap(self.__neurons[:,start:end], border=borderWidth, shareIntensity=shareIntensity, shape="hex")
+        else:
+            print ("quadratic map")
+            image = self.calculateMap(self.__neurons[:,start:end], border=borderWidth, shareIntensity=shareIntensity, shape="box")
+
+        ax = pyplot.subplot()
+        if channel==0:
+            cmap = matplotlib.cm.get_cmap("Blues")
+        elif channel==1:
+            cmap = matplotlib.cm.get_cmap("Reds")
+        elif channel==2:
+            cmap = matplotlib.cm.get_cmap("Greens")
+        elif channel==3:
+            cmap = matplotlib.cm.get_cmap("Purples")
+        elif channel==4:
+            cmap = matplotlib.cm.get_cmap("Greys")
+        elif channel==5:
+            cmap = matplotlib.cm.get_cmap("Oranges")
+        else:
+            cmap = matplotlib.cm.get_cmap()
+        cmap.set_bad(facecolor,1.)
+        ax.imshow(image.T, aspect='auto', interpolation="nearest", cmap=cmap)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.axis("off")
+        fileName = save+'/'+name+'%d.pdf' %channel
+        pyplot.savefig(fileName)
+        if(display==1):
+            pyplot.show()
+
 
 if __name__ == "__main__":
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hc:",["channel=", "help", "hex"])
+        opts, args = getopt.getopt(sys.argv[1:],"h:s:b:c:i:n:d:",["help", "save=", "border=", "color=", "intensity=", "name=", "display="])
     except getopt.GetoptError:
         print_usage()
         sys.exit(1)
 
-    imageNumber = 0
-    channelNumber = 0
-    hex = 0
+    #Default parameters
+    save=""
+    name="SOM"
+    borderWidth=2
+    facecolor="#ffaadd"
+    shareIntensity=True
+    display=0
 
+    #Use inputted parameters
     for opt, arg in opts:
-        if opt in ("-c", "--channel"):
-            channelNumber = int(arg)
+        if opt in ("-s", "--save"):
+            save = arg
+        elif opt in ("-i", "--shareIntensity"):
+            shareIntensity = arg
+        elif opt in ("-c", "--color"):
+            facecolor = arg
+        elif opt in ("-b", "--border"):
+            borderWidth = int(arg)
+        elif opt in ("-n", "--name"):
+            name = arg
+        elif opt in ("-d", "--display"):
+            display = int(arg)
         elif opt in ("-h", "--help"):
             print_usage()
             sys.exit()
-            channelNumber = int(arg)
-        elif opt in ("--hex"):
-            hex = 1
-
+            
     if len(args) != 1:
         print_usage()
-        print 'ERROR: Input file is missing.'
+        print ('ERROR: Input file is missing.')
         sys.exit(1)
 
     inputfile = args[0]
 
-    print 'Input file is ', inputfile
-    print 'Channel number is ', channelNumber
-
-    inFile = open(inputfile, 'rb')
-    numberOfChannels, SOM_width, SOM_height, SOM_depth, neuron_width, neuron_height = struct.unpack('i' * 6, inFile.read(4*6))
-
-    print 'Number of channels = ', numberOfChannels
-    print 'SOM_width = ', SOM_width
-    print 'SOM_height = ', SOM_height
-    print 'SOM_depth = ', SOM_depth
-    print 'neuron_width = ', neuron_width
-    print 'neuron_height = ', neuron_height
-
-    if channelNumber >= numberOfChannels:
-        print 'Channel number too large.'
-        sys.exit(1)
-
-    SOM_size = SOM_width * SOM_height * SOM_depth
-    if hex:
-        radius = (SOM_width - 1)/2;
-        SOM_size -= radius * (radius + 1);
-
-    print 'SOM_size = ', SOM_size
-
-    dataSize = numberOfChannels * SOM_size * neuron_width * neuron_height
-    array = numpy.array(struct.unpack('f' * dataSize, inFile.read(dataSize * 4)))
-
-    image_width = SOM_width * neuron_width
-    image_height = SOM_depth * SOM_height * neuron_height
-    data = numpy.ndarray([SOM_width, SOM_height, SOM_depth, numberOfChannels, neuron_width, neuron_height], 'float', array)
-    data = numpy.swapaxes(data, 0, 5) # neuron_height, SOM_height, SOM_depth, numberOfChannels, neuron_width, SOM_width
-    data = numpy.swapaxes(data, 0, 2) # SOM_depth, SOM_height, neuron_height, numberOfChannels, neuron_width, SOM_width
-    data = numpy.swapaxes(data, 4, 5) # SOM_depth, SOM_height, neuron_height, numberOfChannels, SOM_width, neuron_width
-    data = numpy.reshape(data, (image_height, numberOfChannels, image_width))
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.set_aspect('equal')
-    plt.imshow(data[:,channelNumber,:], interpolation='nearest', cmap=plt.cm.jet)
-    plt.colorbar()
-    plt.show()
-
-    print 'All done.'
-    sys.exit()
+    print ('Input file is:', inputfile)
+    print ('Save location is:', save)
+    print ('The file name is:', name)
+    print ('The border width is:', borderWidth)
+    print ('The background color is:', facecolor)
+    print ('The intensity is shared:', shareIntensity)
+    if(display==1):
+        print ('Display is on')
+    else:
+        print ('Display is off')
+     
+    #Importing here allows control of the backend depending on if display is on or off.
+    if(display==0):
+        matplotlib.use('Agg')
+    from matplotlib import pyplot
+   
+    myVisualizer = MAPVisualizer(inputfile)
+    myVisualizer.readMap()
+    channels=myVisualizer.checkChannels()
+    for i in range(0,channels):
+        myVisualizer.showMap(i, shareIntensity, borderWidth, facecolor)
