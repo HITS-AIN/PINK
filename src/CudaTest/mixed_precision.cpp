d466ca07a27f8ced32cd41217b717e4c47600ac8
Bernd Doser <bernd.doser@h-its.org>
2018-04-16 14:47:28 +0000

    prpare test for mixed precision

---
+++ src/CudaTest/mixed_precision.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file   CudaTest/mixed_precision.cpp
+ * @date   Apr 16, 2018
+ * @author Bernd Doser <bernd.doser@h-its.org>
+ */
+
+#include "gtest/gtest.h"
+
+TEST(mixed_precision, int8)
+{
+    EXPECT_EQ(1, 1);
+}
