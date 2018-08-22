11d184d6d447288664c9f086954ece52aa272376
Bernd Doser <bernd.doser@h-its.org>
2018-08-22 09:32:01 +0000

    add conan support

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conans import ConanFile, CMake
+
+class RecordConan(ConanFile):
+    
+    name = "pink"
+    version = "1.0"
+    license = "GPLv3"
+    description = "Parallelized rotation and flipping INvariant Kohonen maps"
+    homepage = "https://github.com/HITS-AIN/PINK"
+    url = "https://github.com/HITS-AIN/PINK.git"
+    
+    exports_sources = "include/*", "test/*", "CMakeLists.txt"
+    no_copy_source = True
+    
+    settings = "os", "compiler", "build_type", "arch"
+    requires = \
+        "gtest/1.8.0@bincrafters/stable"
+    generators = "cmake"
+    default_options = "Boost:header_only=True"
+
+    def build(self):
+        # This is not building a library, just test
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()
