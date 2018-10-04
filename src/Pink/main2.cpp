e06378047c037ef8293d12c246435fd315c914ff
Bernd Doser <bernd.doser@h-its.org>
2018-10-04 11:46:56 +0000

    fix main2

--- src/Pink/main2.cpp
+++ src/Pink/main2.cpp
@@ -39,41 +39,41 @@ int main(int argc, char **argv)
 
     InputData input_data(argc, argv);
 
-    if (input_data.layaut == Layout::CARTESIAN)
-        if (input_data.dimensionality == 1)
-        	main_generic<CartesianLayout<1>, CartesianLayout<2>, float>();
-        else if (input_data.dimensionality == 2)
-			main_generic<CartesianLayout<2>, CartesianLayout<2>, float>();
-        else if (input_data.dimensionality == 3)
-			main_generic<CartesianLayout<3>, CartesianLayout<2>, float>();
-        else
-        	std::runtime_error("Unsupported dimensionality of " + input_data.dimensionality);
-    else if (input_data.layaut == Layout::HEXAGONAL)
-    	main_generic<HexagonalLayout, CartesianLayout<2>, float>();
-    else
-    	std::runtime_error("Unknown layout");
-
-	if (input_data.executionPath == ExecutionPath::TRAIN)
-	{
-        Trainer trainer(
-            GaussianFunctor(1.1, 0.2),
-            input_data.verbose,
-			input_data.numberOfRotations,
-			input_data.useFlip,
-			input_data.progressFactor,
-			input_data.useCuda,
-			input_data.maxUpdateDistance
-        );
-        for (auto&& iter_image_cur = ImageIterator<float>(input_data.imagesFilename), iter_image_end = ImageIterator<float>();
-        	iter_image_cur != iter_image_end; ++iter_image_cur)
-        {
-        	Cartesian<2, float> image({3, 3}, iter_image_cur->getPointerOfFirstPixel());
-            trainer(som, image);
-        }
-	} else if (input_data.executionPath == ExecutionPath::MAP) {
-		//Mapper mapper;
-	} else
-    	std::runtime_error("Unknown execution path");
+//    if (input_data.layaut == Layout::CARTESIAN)
+//        if (input_data.dimensionality == 1)
+//        	main_generic<CartesianLayout<1>, CartesianLayout<2>, float>();
+//        else if (input_data.dimensionality == 2)
+//			main_generic<CartesianLayout<2>, CartesianLayout<2>, float>();
+//        else if (input_data.dimensionality == 3)
+//			main_generic<CartesianLayout<3>, CartesianLayout<2>, float>();
+//        else
+//        	std::runtime_error("Unsupported dimensionality of " + input_data.dimensionality);
+//    else if (input_data.layaut == Layout::HEXAGONAL)
+//    	main_generic<HexagonalLayout, CartesianLayout<2>, float>();
+//    else
+//    	std::runtime_error("Unknown layout");
+//
+//	if (input_data.executionPath == ExecutionPath::TRAIN)
+//	{
+//        Trainer trainer(
+//            GaussianFunctor(1.1, 0.2),
+//            input_data.verbose,
+//			input_data.numberOfRotations,
+//			input_data.useFlip,
+//			input_data.progressFactor,
+//			input_data.useCuda,
+//			input_data.maxUpdateDistance
+//        );
+//        for (auto&& iter_image_cur = ImageIterator<float>(input_data.imagesFilename), iter_image_end = ImageIterator<float>();
+//        	iter_image_cur != iter_image_end; ++iter_image_cur)
+//        {
+//        	Cartesian<2, float> image({3, 3}, iter_image_cur->getPointerOfFirstPixel());
+//            trainer(som, image);
+//        }
+//	} else if (input_data.executionPath == ExecutionPath::MAP) {
+//		//Mapper mapper;
+//	} else
+//    	std::runtime_error("Unknown execution path");
 
     // Stop and print timer
     auto&& stopTime = myclock::now();
