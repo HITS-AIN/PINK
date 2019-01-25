730d56ef46de3a6ed92c50f213d463acdfa5d4b7
Bernd Doser <bernd.doser@h-its.org>
2019-01-25 16:29:13 +0100

    test rotate2

--- test/ImageProcessingTest/rotate.cpp
+++ test/ImageProcessingTest/rotate.cpp
@@ -13,6 +13,13 @@
 
 using namespace pink;
 
+template <typename T>
+std::ostream& operator << (std::ostream& os, std::vector<T> v)
+{
+	for (auto e : v) os << e << ", ";
+	return os;
+}
+
 TEST(RotationTest, rotate)
 {
 	int src_height = 1;
@@ -28,5 +35,23 @@ TEST(RotationTest, rotate)
 
     std::vector<float> est{0.0, 0.0857864, 0.0, 0.0857864, 1.0, 0.0857864, 0.0, 0.0857864, 0.0};
     EXPECT_TRUE(EqualFloatArrays(est, dst, 1e-4));
+}
 
+TEST(RotationTest, rotate2)
+{
+	int src_height = 2;
+	int src_width = 2;
+	int dst_height = 3;
+	int dst_width = 3;
+	float rad = 0.25 * M_PI;
+
+    std::vector<float> src(src_height * src_width, 1.0);
+    std::vector<float> dst(dst_height * dst_width, 0.0);
+
+    rotate_bilinear(&src[0], &dst[0], src_height, src_width, dst_height, dst_width, rad);
+
+    std::cout << dst << std::endl;
+
+    std::vector<float> est{0.0857865, 0.62868, 0.0857865, 0.62868, 1, 0.62868, 0.0857865, 0.62868, 0.0857865};
+    EXPECT_TRUE(EqualFloatArrays(est, dst, 1e-4));
 }
