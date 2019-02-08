0da59300c905f894a68fbd88e36280e712ce6d3f
Bernd Doser <bernd.doser@h-its.org>
2019-02-08 12:41:44 +0100

    Revert "add async streams for multi gpu code"
    This reverts commit 696a1a20f7b49b88265c53e3a6f5cf88e177c80b.

--- src/CudaLib/generate_euclidean_distance_matrix_first_step_multi_gpu.h
+++ src/CudaLib/generate_euclidean_distance_matrix_first_step_multi_gpu.h
@@ -57,8 +57,6 @@ void generate_euclidean_distance_matrix_first_step_multi_gpu(thrust::device_vect
         {
             // Start GPU device
             cudaSetDevice(gpu_ids[i]);
-            cudaStream_t stream;
-            cudaStreamCreate(&stream);
 
             thrust::device_ptr<const EuclideanType> d_som_local_ptr;
             thrust::device_ptr<const EuclideanType> d_rotated_images_local_ptr;
@@ -75,12 +73,12 @@ void generate_euclidean_distance_matrix_first_step_multi_gpu(thrust::device_vect
                 d_first_step_local.resize(size[i] * number_of_spatial_transformations);
 
                 // Copy data
-                gpuErrchk(cudaMemcpyPeerAsync(thrust::raw_pointer_cast(d_som_local.data()), i,
+                gpuErrchk(cudaMemcpyPeer(thrust::raw_pointer_cast(d_som_local.data()), i,
                                     thrust::raw_pointer_cast(d_som.data()) + offset[i] * neuron_size, 0,
-                                    size[i] * neuron_size * sizeof(EuclideanType), stream));
-                gpuErrchk(cudaMemcpyPeerAsync(thrust::raw_pointer_cast(d_rotated_images_local.data()), i,
+                                    size[i] * neuron_size * sizeof(EuclideanType)));
+                gpuErrchk(cudaMemcpyPeer(thrust::raw_pointer_cast(d_rotated_images_local.data()), i,
                                     thrust::raw_pointer_cast(d_rotated_images.data()), 0,
-                                    number_of_spatial_transformations * neuron_size * sizeof(EuclideanType), stream));
+                                    number_of_spatial_transformations * neuron_size * sizeof(EuclideanType)));
 
                 d_som_local_ptr = d_som_local.data();
                 d_rotated_images_local_ptr = d_rotated_images_local.data();
@@ -97,16 +95,16 @@ void generate_euclidean_distance_matrix_first_step_multi_gpu(thrust::device_vect
 
             switch (block_size)
             {
-                case  512: euclidean_distance_kernel< 512><<<dim_grid, dim_block, 0, stream>>>(
+                case  512: euclidean_distance_kernel< 512><<<dim_grid, dim_block>>>(
                     thrust::raw_pointer_cast(d_som_local_ptr), thrust::raw_pointer_cast(d_rotated_images_local_ptr),
                     thrust::raw_pointer_cast(d_first_step_local_ptr), neuron_size); break;
-                case  256: euclidean_distance_kernel< 256><<<dim_grid, dim_block, 0, stream>>>(
+                case  256: euclidean_distance_kernel< 256><<<dim_grid, dim_block>>>(
                     thrust::raw_pointer_cast(d_som_local_ptr), thrust::raw_pointer_cast(d_rotated_images_local_ptr),
                     thrust::raw_pointer_cast(d_first_step_local_ptr), neuron_size); break;
-                case  128: euclidean_distance_kernel< 128><<<dim_grid, dim_block, 0, stream>>>(
+                case  128: euclidean_distance_kernel< 128><<<dim_grid, dim_block>>>(
                     thrust::raw_pointer_cast(d_som_local_ptr), thrust::raw_pointer_cast(d_rotated_images_local_ptr),
                     thrust::raw_pointer_cast(d_first_step_local_ptr), neuron_size); break;
-                case   64: euclidean_distance_kernel<  64><<<dim_grid, dim_block, 0, stream>>>(
+                case   64: euclidean_distance_kernel<  64><<<dim_grid, dim_block>>>(
                     thrust::raw_pointer_cast(d_som_local_ptr), thrust::raw_pointer_cast(d_rotated_images_local_ptr),
                     thrust::raw_pointer_cast(d_first_step_local_ptr), neuron_size); break;
                 default:
@@ -116,14 +114,10 @@ void generate_euclidean_distance_matrix_first_step_multi_gpu(thrust::device_vect
 
             // Copy data
             if (i != 0) {
-                gpuErrchk(cudaMemcpyPeerAsync(thrust::raw_pointer_cast(d_first_step.data()) + offset[i] * number_of_spatial_transformations, 0,
-                    thrust::raw_pointer_cast(d_first_step_local_ptr), i, size[i] * number_of_spatial_transformations * sizeof(DataType), stream));
+                gpuErrchk(cudaMemcpyPeer(thrust::raw_pointer_cast(d_first_step.data()) + offset[i] * number_of_spatial_transformations, 0,
+                    thrust::raw_pointer_cast(d_first_step_local_ptr), i, size[i] * number_of_spatial_transformations * sizeof(DataType)));
+                gpuErrchk(cudaDeviceSynchronize());
             }
-
-            gpuErrchk(cudaStreamSynchronize(stream));
-            gpuErrchk(cudaStreamDestroy(stream));
-            gpuErrchk(cudaDeviceSynchronize());
-
         }));
     }
 
