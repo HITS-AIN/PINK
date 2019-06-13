343eeb8fa4ad5026a29ea526dd40d254ccdc9932
Bernd Doser <bernd.doser@h-its.org>
2019-06-13 11:43:14 +0000

    #Fix 23: --inter-store keep not working
    It was a bug in the ProgressBar. The valid function was not working,
    because next_progress_print was updated within the increment operator.
    This works for printing the progress bar but not for the later valid()
    function, which is used for writing intermediate results.
    
    Fixed by change from comparison with next number to modulus.
    
    Added some unit tests and output stream.

--- src/UtilitiesLib/ProgressBar.h
+++ src/UtilitiesLib/ProgressBar.h
@@ -9,46 +9,50 @@
 #include <chrono>
 #include <iostream>
 
+#include "UtilitiesLib/pink_exception.h"
+
+namespace pink {
+
 class ProgressBar
 {
 public:
 
-    ProgressBar(int total, int width, int number_of_progress_prints = 10)
-     : total(total),
-       width(width),
-       number_of_progress_prints(number_of_progress_prints),
-       next_progress_print(total / number_of_progress_prints)
+    ProgressBar(int total, int width, int number_of_progress_prints = 10, std::ostream& os = std::cout)
+	 : total(total < 1 ? throw pink::exception("ProgressBar: total must be larger than 0") : total),
+       width(width < number_of_progress_prints ? throw pink::exception("ProgressBar: width must be larger than number of progress prints") : width),
+       number_of_progress_prints(number_of_progress_prints < 0 ? throw pink::exception("ProgressBar: width must be equal or larger than 0") : number_of_progress_prints),
+       number_of_ticks_in_section(total / number_of_progress_prints),
+	   os(os)
     {}
 
     void operator ++ ()
     {
         ++ticks;
-        if (valid())
+        if (ticks % number_of_ticks_in_section == 0)
         {
             ++progress;
-            next_progress_print = (progress+1) * total / number_of_progress_prints;
             int pos = width * progress / number_of_progress_prints;
 
             std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
             auto time_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
 
-            std::cout << "[";
+            os << "[";
 
             for (int i = 0; i < width; ++i) {
-                if (i < pos) std::cout << '=';
-                else if (i == pos) std::cout << ">";
-                else std::cout << ' ';
+                if (i < pos) os << '=';
+                else if (i == pos) os << ">";
+                else os << ' ';
             }
-            std::cout << "] " << static_cast<int>(100.0 * progress / number_of_progress_prints) << " % " << time_elapsed / 1000.0 << " s" << std::endl;
+            os << "] " << static_cast<int>(100.0 * progress / number_of_progress_prints) << " % " << time_elapsed / 1000.0 << " s" << std::endl;
 
-            if (ticks == total) std::cout << std::endl;
-            else std::cout << std::flush;
+            if (ticks == total) os << std::endl;
+            else os << std::flush;
         }
     }
 
-    bool valid () const
+    bool valid() const
     {
-        return ticks == next_progress_print;
+        return ticks != 0 and ticks % number_of_ticks_in_section == 0;
     }
 
 private:
@@ -64,13 +68,16 @@ private:
     /// Number of progress informations should be printed
     int number_of_progress_prints;
 
-    /// Number of ticks when the next progress information should be printed
-    int next_progress_print;
+    /// Number of ticks between progress stages
+    int number_of_ticks_in_section;
 
     /// Current progress number
     int progress = 0;
 
+    /// Current progress number
+    std::ostream& os;
+
     std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
 };
 
-
+} // namespace pink
---
+++ test/UtilitiesTest/ProgressBarTest.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file   UtilitiesTest/ProgressBarTest.cpp
+ * @date   Jun 13, 2019
+ * @author Bernd Doser, HITS gGmbH
+ */
+
+#include <cmath>
+#include <gtest/gtest.h>
+
+#include "UtilitiesLib/ProgressBar.h"
+
+using namespace pink;
+
+TEST(ProgressBarTest, valid)
+{
+	std::stringstream ss;
+
+    ProgressBar progress(100, 70, 10, ss);
+
+    EXPECT_FALSE(progress.valid());
+    EXPECT_EQ(ss.str(), "");
+    for (int i = 0; i < 10; ++i) ++progress;
+    EXPECT_TRUE(progress.valid());
+    EXPECT_EQ(ss.str(), "[=======>                                                              ] 10 % 0 s\n");
+}
+
+TEST(ProgressBarTest, invalid_construction)
+{
+    EXPECT_THROW(ProgressBar(0, 0, 0), pink::exception);
+
+    EXPECT_THROW(ProgressBar(-10,  10,  10), pink::exception);
+    EXPECT_THROW(ProgressBar( 10, -10,  10), pink::exception);
+    EXPECT_THROW(ProgressBar( 10,  10, -10), pink::exception);
+
+    EXPECT_THROW(ProgressBar(100, 9, 10), pink::exception);
+}
