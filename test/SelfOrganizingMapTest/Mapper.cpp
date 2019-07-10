7a917099c380336506e883d28045ea53f6757be5
Bernd Doser <bernd.doser@h-its.org>
2019-07-10 14:08:32 +0200

    Check d(A,B) == d(B,A)

--- test/SelfOrganizingMapTest/Mapper.cpp
+++ test/SelfOrganizingMapTest/Mapper.cpp
@@ -21,13 +21,16 @@ using namespace pink;
 
 struct MapperTestData
 {
-	MapperTestData(int som_dim, int neuron_dim, int image_dim, int euclidean_distance_dim, int num_rot, bool flip, std::vector<float> result)
+	MapperTestData(int som_dim, int neuron_dim, int image_dim, int euclidean_distance_dim, int num_rot, bool flip,
+		float neuron_value, float image_value, std::vector<float> result)
       : som_dim(som_dim),
 		neuron_dim(neuron_dim),
 		image_dim(image_dim),
 		euclidean_distance_dim(euclidean_distance_dim),
 		num_rot(num_rot),
 		flip(flip),
+		neuron_value(neuron_value),
+		image_value(image_value),
 		result(result),
         som_size(som_dim * som_dim),
         neuron_size(neuron_dim * neuron_dim),
@@ -42,6 +45,9 @@ struct MapperTestData
     int num_rot;
     bool flip;
     std::vector<float> result;
+    float neuron_value;
+    float image_value;
+
     int som_size;
     int neuron_size;
     int image_size;
@@ -60,8 +66,8 @@ TEST_P(MapperTest, mapper_cartesian_2d_float)
 
     auto data = GetParam();
 
-    DataType image({data.image_dim, data.image_dim}, std::vector<float>(data.image_size, 0.0));
-    SOMType som({data.som_dim, data.som_dim}, {data.neuron_dim, data.neuron_dim}, std::vector<float>(data.som_total_size, 1.0));
+    DataType image({data.image_dim, data.image_dim}, std::vector<float>(data.image_size, data.image_value));
+    SOMType som({data.som_dim, data.som_dim}, {data.neuron_dim, data.neuron_dim}, std::vector<float>(data.som_total_size, data.neuron_value));
 
     MapperType mapper(som, 0, data.num_rot, data.flip, Interpolation::BILINEAR, data.euclidean_distance_dim);
     auto result = mapper(image);
@@ -71,11 +77,19 @@ TEST_P(MapperTest, mapper_cartesian_2d_float)
 
 INSTANTIATE_TEST_CASE_P(MapperTest_all, MapperTest,
     ::testing::Values(
-    	// som_dim, neuron_dim, image_dim, euclidean_distance_dim, num_rot, flip, result
-        MapperTestData(1, 2, 2, 2,   1, false, {4.0}),
-        MapperTestData(1, 3, 3, 2,   1, false, {4.0}),
-        MapperTestData(1, 3, 3, 2, 360, false, {4.0}),
-        MapperTestData(2, 3, 3, 2, 360, false, {4.0, 4.0, 4.0, 4.0}),
-        MapperTestData(2, 3, 3, 2, 360,  true, {4.0, 4.0, 4.0, 4.0}),
-        MapperTestData(2, 3, 5, 2, 360,  true, {4.0, 4.0, 4.0, 4.0})
+    	// som_dim, neuron_dim, image_dim, euclidean_distance_dim, num_rot, flip, neuron_value, image_value, result
+        MapperTestData(1, 2, 2, 2,   1, false, 0.0, 1.0, {4.0}),
+        MapperTestData(1, 3, 3, 2,   1, false, 0.0, 1.0, {4.0}),
+        MapperTestData(1, 3, 5, 2, 360, false, 0.0, 1.0, {4.0}),
+        MapperTestData(2, 3, 5, 2, 360, false, 0.0, 1.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(2, 3, 5, 2, 360,  true, 0.0, 1.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(2, 3, 5, 2, 360,  true, 0.0, 1.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(1, 2, 2, 2,   1, false, 1.0, 0.0, {4.0}),
+        MapperTestData(1, 3, 3, 2,   1, false, 1.0, 0.0, {4.0}),
+        MapperTestData(1, 3, 5, 2, 360, false, 1.0, 0.0, {4.0}),
+        MapperTestData(2, 3, 5, 2, 360, false, 1.0, 0.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(2, 3, 5, 2, 360,  true, 1.0, 0.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(2, 3, 5, 2, 360,  true, 1.0, 0.0, {4.0, 4.0, 4.0, 4.0}),
+        MapperTestData(1, 2, 2, 2,   1, false, 0.0, 0.5, {1.0}),
+        MapperTestData(1, 2, 2, 2,   1, false, 0.5, 0.0, {1.0})
 ));
