6f776b5a558db1b2dac94772be58dc324890ba97
Bernd Doser <bernd.doser@h-its.org>
2019-07-12 13:45:00 +0200

    add tests for CartesianLayout get_distance

--- test/SelfOrganizingMapTest/Cartesian.cpp
+++ test/SelfOrganizingMapTest/Cartesian.cpp
@@ -16,4 +16,10 @@ TEST(CartesianLayoutTest, cartesian_2d)
 {
     CartesianLayout<2> c{10, 10};
     EXPECT_EQ(100, c.size());
+
+    EXPECT_EQ(0.0, c.get_distance({0, 0}, {0, 0}));
+    EXPECT_EQ(1.0, c.get_distance({0, 0}, {0, 1}));
+    EXPECT_EQ(2.0, c.get_distance({0, 0}, {0, 2}));
+    EXPECT_NEAR(std::sqrt(2.0), c.get_distance({0, 0}, {1, 1}), 1e-7);
+    EXPECT_NEAR(std::sqrt(8.0), c.get_distance({0, 0}, {2, 2}), 1e-7);
 }
