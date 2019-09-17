51fac0d48c7bf5fd6243d5ce655826dccafeeeb3
Bernd Doser <bernd.doser@h-its.org>
2019-09-17 17:01:19 +0200

    Test hexagonal DynamicTrainer

--- test/PythonBindingTest/DynamicTrainer.cpp
+++ test/PythonBindingTest/DynamicTrainer.cpp
@@ -32,3 +32,22 @@ TEST(GenericPythonBindingTest, DynamicTrainer)
 
     trainer(data);
 }
+
+TEST(GenericPythonBindingTest, DynamicTrainer_hex)
+{
+    std::vector<ssize_t> shape{3, 3};
+    std::vector<float> ptr{2.0f, 3.0f, 0.0f, 1.0f};
+
+    DynamicData data("float32", "cartesian-2d", shape, static_cast<void*>(&ptr[0]));
+
+    std::vector<uint32_t> som_shape{3, 3, 3};
+    std::vector<float> som_ptr(7, 0.0f);
+
+    DynamicSOM som("float32", "hexagonal-2d", "cartesian-2d", som_shape, static_cast<void*>(&som_ptr[0]));
+
+    auto&& f = GaussianFunctor(1.1f, 0.2f);
+
+    DynamicTrainer trainer(som, f, 0, 16, true, -1.0, Interpolation::BILINEAR, true, 3, DataType::UINT8);
+
+    trainer(data);
+}
