8dc8f77b84c22ae5c528b1b2a83bddff9d52f1e3
Bernd Doser <bernd.doser@h-its.org>
2020-03-10 15:44:44 +0100

    add cart-2d circular-ed

---
+++ src/ImageProcessingLib/circular_euclidean_distance.h
@@ -0,0 +1,51 @@
+/**
+ * @file   ImageProcessingLib/circular_euclidean_distance.h
+ * @date   Mar 10, 2020
+ * @author Bernd Doser, HITS gGmbH
+ */
+
+#pragma once
+
+#include <cassert>
+
+#include "SelfOrganizingMapLib/CartesianLayout.h"
+
+namespace pink {
+
+/// Primary template for CircularEuclideanDistanceFunctor
+template <typename DataLayout>
+struct CircularEuclideanDistanceFunctor
+{
+    template <typename T>
+    T operator () (T const *a, T const *b, DataLayout const& data_layout) const;
+};
+
+/// EuclideanDistanceFunctor: Specialization for CartesianLayout<2>
+template <>
+struct CircularEuclideanDistanceFunctor<CartesianLayout<2>>
+{
+    template <typename T>
+    T operator () (T const *a, T const *b, CartesianLayout<2> const& data_layout) const
+    {
+        T ed = 0;
+
+        auto dim = data_layout.get_dimension(0);
+        auto radius = dim / 2;
+        auto radius_squared = radius * radius;
+
+        for (uint32_t i = 0; i < dim; ++i) {
+            for (uint32_t j = 0; j < dim; ++j) {
+                auto dx = x - radius;
+                auto dy = y - radius;
+                auto distance_squared = dx * dx + dy * dy;
+
+                if (distance_squared <= radius_squared) {
+                    ed += std::pow(a[i * dim + j] - b[i * dim + j], 2);
+                }
+            }
+        }
+        return ed;
+    }
+};
+
+} // namespace pink
