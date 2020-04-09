1ca804821619dad373bf7c0c96a6e52b1e85ad44
Bernd Doser <bernd.doser@h-its.org>
2020-04-09 17:08:30 +0200

    test rot90 of circular ED

--- test/SelfOrganizingMapTest/circular_ed.cpp
+++ test/SelfOrganizingMapTest/circular_ed.cpp
@@ -27,12 +27,13 @@ TEST(SelfOrganizingMapTest, circular_ed)
 
     auto image_dim = 4U;
     auto image_vec = std::vector<float>(image_dim * image_dim, 0.0);
-    image_vec[5] = 1.0;
+    image_vec[4] = 1.0;
 
     auto som_dim = 2U;
     auto neuron_dim = 4U;
     auto som_vec = std::vector<float>(som_dim * som_dim * neuron_dim * neuron_dim, 0.0);
-    som_vec[5] = 1.0;
+    som_vec[4] = 1.0; // [0, 0, 1, 0]
+    som_vec[18] = 0.5; // [0, 1, 0, 2]
 
     DataType image({image_dim, image_dim}, image_vec);
     SOMType som({som_dim, som_dim}, {neuron_dim, neuron_dim}, som_vec);
@@ -44,6 +45,6 @@ TEST(SelfOrganizingMapTest, circular_ed)
     MapperType mapper(som, 0, num_rot, flip, Interpolation::BILINEAR, euclidean_distance_dim, EuclideanDistanceShape::CIRCULAR);
     auto result = mapper(image);
 
-    EXPECT_EQ((std::vector<float>{0.0, 1.0, 1.0, 1.0}), std::get<0>(result));
-    EXPECT_EQ((std::vector<uint32_t>{0, 0, 0, 0}), std::get<1>(result));
+    EXPECT_EQ((std::vector<float>{0.0, 0.5, 1.0, 1.0}), std::get<0>(result));
+    EXPECT_EQ((std::vector<uint32_t>{0, 1, 0, 0}), std::get<1>(result));
 }
