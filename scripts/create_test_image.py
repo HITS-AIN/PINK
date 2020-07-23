245b8bb4ec134303422f6e8336fc1906f9ffcef5
Bernd Doser <bernd.doser@h-its.org>
2020-07-23 17:45:38 +0200

    add diag fill option for test images

--- scripts/create_test_image.py
+++ scripts/create_test_image.py
@@ -9,6 +9,7 @@ __email__ = "bernd.doser@h-its.org"
 __license__ = "GPLv3"
 
 import argparse
+import numpy as np
 import random
 import os
 import struct
@@ -22,7 +23,7 @@ def main():
     parser.add_argument('--width', type=int, default=64, help='Image width (default = 64)')
     parser.add_argument('--height', type=int, default=64, help='Image height (default = 64)')
     parser.add_argument('--depth', type=int, default=1, help='Image depth (default = 1)')
-    parser.add_argument('--fill-with', choices=['random', 'zero', 'one'], default='random', help='Pixel value (random=default, zero, one)')
+    parser.add_argument('--fill-with', choices=['random', 'zero', 'one', 'diag'], default='random', help='Pixel value (random=default, zero, one)')
     parser.add_argument('--file-version', choices=['1', '2'], default='2', help='File version (1, 2 = default)')
     args = parser.parse_args()
 
@@ -33,9 +34,9 @@ def main():
         output.write(struct.pack('i' * 4, args.number_of_images, 1, args.width, args.height))
     elif args.file_version == '2':
         if args.depth == 1:
-            output.write(struct.pack('i' * 8, 2, 0, 0, args.number_of_images, 0, 2, args.width, args.height))
+            output.write(struct.pack('i' * 8, 2, 0, 0, args.number_of_images, 0, 2, args.height, args.width))
         else:
-            output.write(struct.pack('i' * 9, 2, 0, 0, args.number_of_images, 0, 3, args.width, args.height, args.depth))
+            output.write(struct.pack('i' * 9, 2, 0, 0, args.number_of_images, 0, 3, args.depth, args.height, args.width))
     
     number_of_pixels = args.number_of_images * args.width * args.height * args.depth
     if args.fill_with == 'random':
@@ -46,6 +47,12 @@ def main():
         output.write(struct.pack('%sf' % len(floatlist), *floatlist))
     elif args.fill_with == 'one':
         floatlist = [1.0 for _ in range(number_of_pixels)]
+        output.write(struct.pack('%sf' % len(floatlist), *floatlist))
+    elif args.fill_with == 'diag':
+        images = np.zeros((args.depth, args.height, args.width)).astype(np.float32)
+        for i in range(args.height):
+            images[0,i,i] = 1
+        floatlist = images.flatten().tolist()
         output.write(struct.pack('%sf' % len(floatlist), *floatlist))
 
     print('All done.')
