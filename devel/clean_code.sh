274b6534958e6ae933853d14f354b539f78dcd91
Bernd Doser <bernd.doser@h-its.org>
2023-06-27 07:06:37 +0000

    remove deprecated devel containers

--- devel/clean_code.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find .. -type f \( \
-    -name "*.cpp" -o \
-    -name "*.h" -o \
-    -name "*.cu" -o \
-    -name "*.py" -o \
-    -name "*.txt" \) \
-    -exec bash -c 'expand -i -t 4 "$0" | sponge "$0"' {} \;
