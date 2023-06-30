055b2d7905a112a184e0f1689487331d6eda0fda
Bernd Doser <bernd.doser@h-its.org>
2023-06-30 17:50:53 +0000

    test pure python pink module

--- python/test/test_pink.py
+++ python/test/test_pink.py
@@ -1,3 +1,16 @@
+import numpy
+print(numpy.__version__)
+
+import pkgutil
+print([name for _, name, _ in pkgutil.iter_modules(['numpy'])])
+
 import pink
+help(pink)
+print([name for _, name, _ in pkgutil.iter_modules(['pink'])])
+
+from pink import tools
+
+input_stream = open("README.md", 'rb')
+tools.ignore_header_comments(input_stream)
 
 print(pink.__version__)
